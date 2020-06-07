fe3165781732320eed3b82150a3242ee410d64aa
Ronaldo Serafim <ronaldo.serafim@outlook.com.br>
2020-06-07 06:37:22 -0300

    Estudos direcionados a Javascript

---
+++ tipagemfraca.js
@@ -0,0 +1,14 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer =3.14
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// evitar nome genéricos e siglas
+
+let valor = ' '
+let numero = 1 
+let pqp = false // Produto Quimico Perigoso... kkk
+
---
+++ variaveisEConstantes.js
@@ -0,0 +1,19 @@
+var a = 3
+let b = 4
+
+var a = 30
+// let b = 40 redeclarar a váriavel 'b' utilizando a palavra reservada 'let' irá acusar problema
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+
+console.log(a, b)
+
+const c = 5
+
+// c = 50
+
+console.log(c)
